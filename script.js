df77cdfb1ddecef1c016731d2d1dbed4c8846c97
alie <anthonylie94@gmail.com>
2022-12-09 16:26:10 +0800

    first UI

---
+++ script.js
@@ -0,0 +1,22 @@
+function Roll(Num)) { 
+if ()
+
+    let roll =Math.floor(Math.random() * 6) + 1; 
+    
+    alert("Rolled " + roll );
+};
+
+function Roll10() {
+
+
+var D = document.getElementById.bind(D6);
+var Die10 = document.getElementById.bind(D10);
+
+if (Die6) { 
+
+    Roll6();   
+}
+
+else if (Die10) {
+    Roll10();
+}
